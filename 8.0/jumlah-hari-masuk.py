578bc4cf88167eac9fc1d46f6b8564e8b1d96524
Andhitia Rama <andhitia.r@gmail.com>
2022-02-07 08:43:31 +0700

    [8.0] Revisi jumlah hari masuk

--- 8.0/jumlah-hari-masuk.py
+++ 8.0/jumlah-hari-masuk.py
@@ -1,6 +1,13 @@
 # Mencari jumlah hari karyawan melakukan absensi tidak lengkap
 # Absensi tidak lengkap terjadi dimana karyawan hanya sign in atau sign out
 
+# DEFINISI
+
+USE_MIN_WORK_HOUR = False
+MIN_WORK_HOUR = 0.0
+
+
+# PERHITUNGAN (JANGAN DIUBAH)
 
 obj_schedule = env["hr.timesheet_attendance_schedule"]
 
@@ -10,6 +17,10 @@ def _cari_hari():
         ("sheet_id", "=", sheet.id),
         ("state", "=", "present")
     ]
+    if USE_MIN_WORK_HOUR:
+        criteria += [
+        ("real_work_hour", ">=", MIN_WORK_HOUR),
+        ]
     return obj_schedule.search_count(criteria)
 
 
